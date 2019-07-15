a438d326df1b29be56cfd5e5906ec779065f6c7c
sho520 <shoa.xyz@gmail.com>
2019-07-15 15:38:41 +0900

    add カラム

--- db/migrate/20190715052917_create_locations.rb
+++ db/migrate/20190715052917_create_locations.rb
@@ -3,6 +3,7 @@ class CreateLocations < ActiveRecord::Migration[5.2]
     create_table :locations do |t|
 
       t.timestamps
+      t.string :prefecture,
     end
   end
 end
--- db/migrate/20190715052942_create_employee_types.rb
+++ db/migrate/20190715052942_create_employee_types.rb
@@ -3,6 +3,9 @@ class CreateEmployeeTypes < ActiveRecord::Migration[5.2]
     create_table :employee_types do |t|
 
       t.timestamps
+      t.string :full_time
+      t.string :part_time
+      t.string :dispatch
     end
   end
 end
