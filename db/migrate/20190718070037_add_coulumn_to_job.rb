4e982dbf80a6818a394866d0035600538ab5e8e3
Kazuaki-Tsuruta <turutakazuaki@icloud.com>
2019-07-19 20:14:15 +0900

    revise_migration_file

--- db/migrate/20190718070037_add_coulumn_to_job.rb
+++ db/migrate/20190718070037_add_coulumn_to_job.rb
@@ -3,7 +3,7 @@ class AddCoulumnToJob < ActiveRecord::Migration[5.2]
     change_column :jobs, :industry_id, :integer, {foreign_key: true}
     change_column :jobs, :number_of_employee_id, :integer, {foreign_key: true}
     change_column :jobs, :location_id, :integer, {foreign_key: true}
-    change_column :jobs, :employment_type_id, :integer, {foreign_key: true}
+    change_column :jobs, :employee_type_id, :integer, {foreign_key: true}
     change_column :jobs, :job_state_id, :integer, {foreign_key: true}
   end
 end
