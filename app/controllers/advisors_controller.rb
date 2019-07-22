22ef8f062a1da41888bef167a4275c56d29afe4e
Kazuaki-Tsuruta <turutakazuaki@icloud.com>
2019-07-22 11:46:21 +0900

    サーバーサイド強化の夜1

--- app/controllers/advisors_controller.rb
+++ app/controllers/advisors_controller.rb
@@ -1,12 +1,7 @@
 class AdvisorsController < ApplicationController
 
-  def show
-    @aaa = "aaa"
-  end
-
-  def students
+  def students                   #生徒一覧ページ。CAのみ閲覧可能。CAのヘッダーから移動可能
     @students = Student.all
   end
-
 
 end
