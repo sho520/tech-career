de6c4b8f347391dce67421d5f7e1d0443ac04f80
S <42575166+shinpei555@users.noreply.github.com>
2019-07-24 04:47:36 +0900

    企業一覧view実装

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -1,5 +1,11 @@
 class ChatsController < ApplicationController
 
+  def index
+    @jobs = Job.all
+    @newjobs = Job.all.order(id: "DESC")
+    @chats = Chat.new
+    @images = JobImage.all
+  end
 
   def new
     @chat = Chat.new
