7cb9905eb594bf1a39a8b9d25a2ba21fed541592
Kazuaki-Tsuruta <turutakazuaki@icloud.com>
2019-07-24 14:34:22 +0900

    生徒マイページにてCA推薦中と検討中の変更を可能にした

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
     get 'all', to: 'jobs#all', on: :collection   #job一覧ページ
     get 'keep', to: 'jobs#keep', on: :member     #jobの選考状況を検討中へ変更(生徒のmyページにて)
     get 'apply', to: 'jobs#apply', on: :member   #jobの選考状況を応募へ変更(生徒のmyページにて)
+    get 'consider', to: 'jobs#consider', on: :member   #jobの選考状況を検討中へ変更(生徒のmyページにて)
+    get 'decline', to: 'jobs#decline', on: :member   #jobの選考状況を検討中へ変更(生徒のmyページにて)
     get 'change', to: 'jobs#change', on: :member #jobの選考状況を変更(求人詳細にて)
     get 'state', to: 'jobs#state', on: :member   #jobの応募状況を変更(求人詳細にて)
   end
