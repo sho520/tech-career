fa5c947e19ff0e27946b89642746c3c437ff4cfc
S <42575166+shinpei555@users.noreply.github.com>
2019-07-29 09:11:57 +0900

    Capistranoのため修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                                keys: ['~/.ssh/career.pem']
+                                keys: ['~/.ssh/techjob.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
