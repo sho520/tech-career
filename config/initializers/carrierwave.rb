cc7e28fe847dd81e1d618e5d4da46a57dec4a3a5
S <42575166+shinpei555@users.noreply.github.com>
2019-08-08 03:51:57 +0900

    S3再度

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -15,3 +15,15 @@ require 'carrierwave/storage/fog'
 #   config.fog_directory  = 'techcareer0805'
 #   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/techcareer0805'
 # end
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
+    region: 'ap-northeast-1'
+  }
+  config.fog_directory  = 'techcareerimg'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/techcareerimg'
+end
